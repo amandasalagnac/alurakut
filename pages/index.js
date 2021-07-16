7fb438a59a0066fc51cf1fb4c2120cf5156d74fb
Amanda Salagnac <amanda.salagnac@gmail.com>
2021-07-16 00:49:39 -0300

    adicionando aula2

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
+import React from "react";
 import MainGrid from "../src/components/MainGrid";
 import Box from "../src/components/Box";
 import {
   AlurakutMenu,
+  AlurakutProfileSidebarMenuDefault,
   OrkutNostalgicIconSet,
 } from "../src/lib/AlurakutCommons";
 import { ProfileRelationsBoxWrapper } from "../src/components/ProfileRelations";
 
 function ProfileSidebar(propriedades) {
-  console.log(propriedades);
   return (
     <Box>
       <img
@@ -15,11 +16,25 @@ function ProfileSidebar(propriedades) {
         style={{ borderRadius: "8px" }}
         alt="Avatar"
       />
+      <hr />
+      <p>
+        <a
+          className="boxLink"
+          href={`https://github.com/${propriedades.githubUser}`}
+        >
+          @{propriedades.githubUser}
+        </a>
+      </p>
+      <hr />
+      <AlurakutProfileSidebarMenuDefault />
     </Box>
   );
 }
 
 export default function Home() {
+  const [comunidades, setCommunities] = React.useState(["Alurakut"]);
+  // comunidades => primeiro item da Array (useState[0])
+  // setCommunities => segundo item da Array (useState[1])
   const usuarioAleatorio = "amandasalagnac";
   const pessoasFavoritas = [
     "juunegreiros",
@@ -42,6 +57,21 @@ export default function Home() {
             <h1 className="title">Bem-vindo(a)</h1>
 
             <OrkutNostalgicIconSet />
+          </Box>
+          <Box>
+            <h2 className="subTitle">O que você deseja fazer?</h2>
+            <form
+              onSubmit={function handleCreateCommunity(event) {
+                event.preventDefault();
+              }}
+            >
+              <button>Criar comunidade</button>
+              <input
+                name="title"
+                placeholder="Qual vai ser o nome da sua comunidade?"
+                aria-label="Qual vai ser o nome da sua comunidade?"
+              />
+            </form>
           </Box>
         </div>
         <div
